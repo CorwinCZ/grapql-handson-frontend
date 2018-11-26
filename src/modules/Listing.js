d3bdbd626a386e0dc157662660bd70f832726be2
Petr Čaněk <canekpet@gmail.com>
2018-11-26 19:37:38 +0100

    Added Query component

--- src/modules/Listing.js
+++ src/modules/Listing.js
@@ -1,66 +1,23 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 
 import ListingItem from './ListingItem';
 
-import apolloClient from '../apolloClient';
-
-class Listing extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [{}, {}],
-    };
-  }
-
-  componentDidMount = async () => {
-    console.log('Mounted');
-
-    const query = gql`
-      query testQuery($itemsCount: Int!) {
-        allPeople(first: $itemsCount) {
-          people {
-            id
-            name
-            mass
-          }
-        }
+const query = gql`
+  query testQuery($itemsCount: Int!) {
+    allPeople(first: $itemsCount) {
+      people {
+        id
+        name
+        mass
       }
-    `;
-
-    const queryVariables = { itemsCount: 4 };
-
-    const resData = await apolloClient.query({
-      query: query,
-      variables: queryVariables,
-    });
-  };
-
-  /*componentDidMount = async () => {
-    const query = `query testQuery($itemsCount: Int!) {
-  allPeople(first: $itemsCount) {
-    people {
-      id
-      name
-      mass
     }
   }
-}`;
-    const queryVariables = { itemsCount: 4 };
+`;
 
-    const responseData = await fetch(
-      `${API_URL}?query=${encodeURIComponent(
-        query,
-      )}&operationName=testQuery&variables=${encodeURIComponent(
-        JSON.stringify(queryVariables),
-      )}`,
-    ).then(response => response.json());
-
-    this.setState({ data: responseData.data.allPeople.people });
-  };*/
-
-  renderDataList = () => {
-    const { data } = this.state;
+class Listing extends Component {
+  renderDataList = data => {
     const { switchToDetail } = this.props;
     return data.map((item, index) => (
       <ListingItem key={index} data={item} switchToDetail={switchToDetail} />
@@ -68,7 +25,23 @@ class Listing extends Component {
   };
 
   render() {
-    return <div>{this.renderDataList()}</div>;
+    const queryVariables = { itemsCount: 3 };
+
+    return (
+      <Query query={query} variables={queryVariables}>
+        {({ data, loading, error }) => {
+          if (error) {
+            return <div>Error!</div>;
+          }
+          if (loading) {
+            return <div>Loading...</div>;
+          }
+
+          console.log('Inside data', data);
+          return this.renderDataList(data.allPeople.people);
+        }}
+      </Query>
+    );
   }
 }
 
