4b7fcb35899e058e9794aeb98a35999b9551f01a
Petr Čaněk <canekpet@gmail.com>
2018-11-26 19:50:58 +0100

    Added query component to detail page

--- src/modules/Detail.js
+++ src/modules/Detail.js
@@ -1,4 +1,15 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+const query = gql`
+  query testPerson($itemId: ID!) {
+    person(id: $itemId) {
+      name
+      eyeColor
+    }
+  }
+`;
 
 class Detail extends Component {
   render() {
@@ -6,7 +17,31 @@ class Detail extends Component {
     if (!itemId) {
       return <div>No ID was given, please provide ID</div>;
     }
-    return <div>This is detail</div>;
+
+    const queryVariables = {
+      itemId: itemId,
+    };
+
+    return (
+      <Query query={query} variables={queryVariables}>
+        {({ data, loading, error }) => {
+          if (error) {
+            return <div>Error!</div>;
+          }
+          if (loading) {
+            return <div>Loading...</div>;
+          }
+          return (
+            <div>
+              name: {data.person.name}
+              <br /> eye color: {data.person.eyeColor}
+            </div>
+          );
+
+          return null;
+        }}
+      </Query>
+    );
   }
 }
 
